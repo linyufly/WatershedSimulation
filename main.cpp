74ebbf5289a46dfc36eabe72861910e29e11038f
linyufly <linyufly@gmail.com>
2014-05-04 20:57:15 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -52,7 +52,7 @@ const int dire[6][3] = {{-1, 0, 0}, {1, 0, 0},
 			{0, 0, -1}, {0, 0, 1}};
 const int numOfNeighbors = 6;
 
-const int laplacianDelta = 0;
+const int laplacianDelta = 4;
 
 double ***ftleValues;
 double spacing[3];
@@ -72,7 +72,7 @@ bool Outside(int x, int y, int z) {
 
 void LoadGrid() {
 	vtkSmartPointer<vtkStructuredPointsReader> reader = vtkSmartPointer<vtkStructuredPointsReader>::New();
-	reader->SetFileName("output_200.vtk");
+	reader->SetFileName("../VincentSoille/output.vtk");
 	reader->Update();
 
 	vtkStructuredPoints *structPoints = reader->GetOutput();
@@ -135,40 +135,175 @@ void LaplacianSmoothing() {
 	printf("Done.\n\n");
 }
 
+bool CheckMinima(int startX, int startY, int startZ, int nextLabel, int ***labels, bool ***visited, CellIndex *queue) {
+	/// DEBUG ///
+	//printf("(%d, %d, %d), nextLabel = %d\n", startX, startY, startZ, nextLabel);
+
+	bool flag = false;
+	int head, tail;
+	queue[0] = CellIndex(startX, startY, startZ);
+	visited[startX][startY][startZ] = true;
+	for (head = tail = 0; head <= tail; head++) {
+		/// DEBUG ///
+		//printf("head = %d, tail = %d\n", head, tail);
+
+		CellIndex currCell = queue[head];
+		for (int k = 0; k < numOfNeighbors; k++) {
+			int _x = currCell.x + dire[k][0];
+			int _y = currCell.y + dire[k][1];
+			int _z = currCell.z + dire[k][2];
+			if (Outside(_x, _y, _z)) continue;
+			if (ftleValues[_x][_y][_z] < ftleValues[startX][startY][startZ]) flag = true;
+
+			if (visited[_x][_y][_z]) continue;
+			if (ftleValues[_x][_y][_z] == ftleValues[startX][startY][startZ]) {
+				visited[_x][_y][_z] = true;
+				queue[++tail] = CellIndex(_x, _y, _z);
+			}
+		}
+	}
+	if (!flag) {
+		for (int i = 0; i <= tail; i++)
+			labels[queue[i].x][queue[i].y][queue[i].z] = nextLabel;
+		return true;
+	}
+	return false;
+}
+
 void Simulation() {
+	printf("Simulation()\n");
+
 	int ***labels = CreateCube<int>(dimensions[0], dimensions[1], dimensions[2]);
 	for (int i = 0; i < dimensions[0]; i++)
 		for (int j = 0; j < dimensions[1]; j++)
 			for (int k = 0; k < dimensions[2]; k++)
 				labels[i][j][k] = -1;
 
-	priority_queue<CellIndex, std::vector<CellIndex>, HeightComparator> heap;
+	bool ***visited = CreateCube<bool>(dimensions[0], dimensions[1], dimensions[2]);
+	for (int i = 0; i < dimensions[0]; i++)
+		for (int j = 0; j < dimensions[1]; j++)
+			for (int k = 0; k < dimensions[2]; k++)
+				visited[i][j][k] = false;
+
+	/// DEBUG ///
+	printf("before finding minima\n");
+
+	int nextLabel = 0;
+
+	CellIndex *queueArray = new CellIndex [dimensions[0] * dimensions[1] * dimensions[2]];
 
 	for (int x = 0; x < dimensions[0]; x++)
 		for (int y = 0; y < dimensions[1]; y++)
-			for (int z = 0; z < dimensions[2]; z++) {
-				if (labels[x][y][z] != -1) continue;
-				bool flag = false;
-				for (int k = 0; k < numOfNeighbors; k++) {
-					int _x = x + dire[k][0];
-					int _y = y + dire[k][1];
-					int _z = z + dire[k][2];
-					if (Outside(_x, _y, _z)) continue;
-					if (ftleValues[_x][_y][_z] < ftleValues[x][y][z]) {
-						flag = true;
-						break;
+			for (int z = 0; z < dimensions[2]; z++)
+				if (!visited[x][y][z])
+					if (CheckMinima(x, y, z, nextLabel, labels, visited, queueArray)) nextLabel++;
+
+	delete [] queueArray;
+
+	/// DEBUG ///
+	printf("nextLabel = %d\n", nextLabel);
+	printf("Finish finding minima\n");
+
+	std::priority_queue<CellIndex, std::vector<CellIndex>, HeightComparator> heap;
+
+	for (int i = 0; i < dimensions[0]; i++)
+		for (int j = 0; j < dimensions[1]; j++)
+			for (int k = 0; k < dimensions[2]; k++)
+				if (labels[i][j][k] != -1) {
+					bool flag = false;
+					for (int d = 0; d < numOfNeighbors; d++) {
+						int _x = i + dire[d][0];
+						int _y = j + dire[d][1];
+						int _z = k + dire[d][2];
+						if (Outside(_x, _y, _z)) continue;
+						if (labels[_x][_y][_z] == -1) {
+							flag = true;
+							break;
+						}
 					}
+					if (flag) heap.push(CellIndex(i, j, k));
 				}
-				if (!flag) {
-					FloodFill
-				}
+
+	while (!heap.empty()) {
+		CellIndex curr = heap.top();
+		heap.pop();
+		for (int k = 0; k < numOfNeighbors; k++) {
+			int _x = curr.x + dire[k][0];
+			int _y = curr.y + dire[k][1];
+			int _z = curr.z + dire[k][2];
+			if (Outside(_x, _y, _z)) continue;
+			if (labels[_x][_y][_z] != -1) continue;
+			if (ftleValues[_x][_y][_z] >= ftleValues[curr.x][curr.y][curr.z]) {
+				labels[_x][_y][_z] = labels[curr.x][curr.y][curr.z];
+				heap.push(CellIndex(_x, _y, _z));
+			}
+		}
+	}
+
+	int *labelColors = new int [nextLabel];
+	for (int i = 0; i < nextLabel; i++)
+		labelColors[i] = i;
+	std::random_shuffle(labelColors, labelColors + nextLabel);
+
+	vtkSmartPointer<vtkImageData> image = vtkSmartPointer<vtkImageData>::New();
+	image->SetDimensions(dimensions);
+	image->SetOrigin(origin);
+	image->SetSpacing(spacing);
+
+#if VTK_MAJOR_VERSION <= 5
+	image->SetNumberOfScalarComponents(1);
+	image->SetScalarTypeToInt();
+#else
+	image->AllocateScalars(VTK_INT, 1);
+#endif
+
+	for (int i = 0; i < dimensions[0]; i++)
+		for (int j = 0; j < dimensions[1]; j++)
+			for (int k = 0; k < dimensions[2]; k++) {
+				int *pixel = static_cast<int *>(image->GetScalarPointer(i, j, k));
+				*pixel = labels[i][j][k];
+				if (labels[i][j][k] >= 0) *pixel = labelColors[*pixel];
 			}
 
+	vtkSmartPointer<vtkXMLImageDataWriter> writer = vtkSmartPointer<vtkXMLImageDataWriter>::New();
+	writer->SetFileName("watershed_output.vtk");	
+
+#if VTK_MAJOR_VERSION <= 5
+	writer->SetInputConnection(image->GetProducerPort());
+#else
+	writer->SetInputData(image);
+#endif
+
+	writer->Write();
+
+	delete [] labelColors;
+
+#if VTK_MAJOR_VERSION <= 5
+	image->SetNumberOfScalarComponents(1);
+	image->SetScalarTypeToDouble();
+#else
+	image->AllocateScalars(VTK_DOUBLE, 1);
+#endif
+
+	for (int i = 0; i < dimensions[0]; i++)
+		for (int j = 0; j < dimensions[1]; j++)
+			for (int k = 0; k < dimensions[2]; k++) {
+				double *pixel = static_cast<double *>(image->GetScalarPointer(i, j, k));
+				*pixel = ftleValues[i][j][k];
+			}
+
+	writer->SetFileName("ftle.vtk");
+	writer->Write();
+
 	DeleteCube(labels);
+	DeleteCube(visited);
+
+	printf("Done\n");
 }
 
 int main() {
 	LoadGrid();
+	LaplacianSmoothing();
 	Simulation();
 	
 	DeleteCube(ftleValues);
